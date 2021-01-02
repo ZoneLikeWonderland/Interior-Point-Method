a1018c9ad77d56b9a7b52626d007c79e373afa13
ZarkLngeW <zhanglw2@shanghaitech.edu.cn>
2021-01-03 07:37:27 +0800

    to mondou2 is good

--- code/main.py
+++ code/main.py
@@ -23,8 +23,7 @@ class IPM:
     gamma = 0.9
     c1 = 1e-4
 
-    alpha_p = None
-    alpha_d = None
+    use_M = False
 
     class Status(Enum):
         TBD = 0
@@ -34,11 +33,27 @@ class IPM:
         UNBOUNDED = 4
         VIOLATED = 5
 
+        @staticmethod
+        def spec(status):
+            return [
+                "TBD",
+                "successfully solved",
+                "hard to solve (cannot converge)",
+                "max iterations exceeded",
+                "feasible but unbounded",
+                "infeasible"
+            ][status.value]
+
     class SD_type(Enum):
         LLT = 0
         LS = 1
 
-    def read_file(self, path: str) -> np.ndarray:
+    def read_file(self, path: str, required: bool = True) -> np.ndarray:
+        if not os.path.exists(path):
+            if not required:
+                return None
+            print("[ ERROR ] could not find", path)
+            exit(-1)
         mat = np.loadtxt(path, delimiter=",", ndmin=2)
         return mat
 
@@ -63,15 +78,12 @@ class IPM:
         x_0 = self.A.T@(AATinv@self.b)
         lam_0 = AATinv@(self.A@self.c)
         s_0 = self.c - self.A.T@lam_0
-        d_x = max(-np.min(x_0), 0)
-        d_s = max(-np.min(s_0), 0)
-        x_0 += d_x
-        s_0 += d_s
+        x_0 += max(-np.min(x_0), 0)
+        s_0 += max(-np.min(s_0), 0)
 
-        u = 1
         x_dot_s = (x_0.T@s_0).item()
-        dh_x = x_dot_s / (s_0.sum()+self.eps) * u+self.eps
-        dh_s = x_dot_s / (x_0.sum()+self.eps) * u+self.eps
+        dh_x = x_dot_s / (s_0.sum()+self.eps)+self.eps
+        dh_s = x_dot_s / (x_0.sum()+self.eps)+self.eps
         x_0 += dh_x
         s_0 += dh_s
         return x_0, lam_0, s_0
@@ -89,7 +101,7 @@ class IPM:
                 if (abs(AT[r, leadj]) > abs(AT[max_index, leadj])):
                     max_index = r
             if (AT[max_index, leadj] == 0):
-                leadi -= 1
+                leadj += 1
                 continue
 
             AT[[leadi, max_index], :] = AT[[max_index, leadi], :]
@@ -108,20 +120,34 @@ class IPM:
             b_t[i] = b[ind_index[i]]
         return A_t, b_t
 
-    def init(self, A_r: Mat, b_r: Vec, c: Vec) -> None:
+    def init(self, A_r: Mat, b_r: Vec, c: Vec, use_M: bool) -> None:
         A, b = self.enforce_fullrank(A_r, b_r)
+        m, n = A.shape
+        if use_M:
+            M = max(np.max(np.abs(c))*n*10, 1)
+            # M = 10000000
+            # M = (np.max(np.abs(A))*np.max(np.abs(b)))**0.5*np.max(np.abs(c))*n
+            print(np.max(np.abs(A)), np.max(np.abs(b)), np.max(np.abs(c)))
+            print("M =", M)
 
+            self.M = M
+
+            A = np.hstack((A, np.identity(m)))
+            c = np.vstack((c, M*np.ones((m, 1))))
+            m, n = A.shape
+            self.use_M = True
+
+        self.m, self.n = m, n
         self.A = ss.csr_matrix(A)
         self.A_dense = A
         self.b = b
         self.c = c
-        self.m, self.n = A.shape
-        m, n = self.m, self.n
 
         self.gF_k = np.zeros((n+m+n, n+m+n))
         self.gF_k[0:n, n:n+m] = A.T
         self.gF_k[n:n+m, 0:n] = A
         self.gF_k[n+m:n+m+n, 0:n] = np.identity(n)
+        self.gF_k = ss.lil_matrix(self.gF_k)
 
     def solve_newton(self, F_d: Vec, F_p: Vec,
                      F_0: Vec, first: bool) -> (Vec, Vec, Vec):
@@ -155,11 +181,11 @@ class IPM:
         alpha = min(np.min(_k / -d_k), 1) * (1 - self.eps)
         return alpha
 
-    def solve(self, detail: bool = False, is_presolve: bool = False) -> Status:
+    def solve(self, args, is_presolve: bool = False, optimal: Mat = None) -> Status:
         total_start_time = time.time()
         status = self.Status.TBD
         m, n = self.m, self.n
-
+        stay = 0
         self.x_k, self.lam_k, self.s_k = self.init_iterate()
 
         for k in range(1, self.max_iterations+1):
@@ -176,7 +202,7 @@ class IPM:
             alpha_d = self.maximum_alpha(self.s_k, ds_k)
 
             mu = (self.x_k.T@self.s_k).item() / n
-            sigma = ((self.x_k+alpha_p*dx_k).T@(self.s_k+alpha_d*ds_k)).item() / n / mu
+            sigma = (((self.x_k+alpha_p*dx_k).T@(self.s_k+alpha_d*ds_k)).item() / n / mu)**2
             toler = mu*sigma / n
 
             F_d = F_k[0:n]
@@ -206,7 +232,7 @@ class IPM:
                 if (alpha_d < self.eps**2 and alpha_p < self.eps**2):
                     break
 
-            if (detail):
+            if (args is not None and args.detail):
                 print("k=%3d,"
                       "f(x)=%5.6f,"
                       "|dx|=%.3e,"
@@ -229,11 +255,12 @@ class IPM:
                 status = self.Status.DONE
                 break
 
-            if (np.linalg.norm(dz_k, np.inf) > 1e31):
+            if (np.linalg.norm(dz_k, np.inf) > 1e10):
                 status = self.Status.UNBOUNDED
                 break
             if (np.linalg.norm((alpha_p * dx_k), np.inf) < self.eps and
                     np.linalg.norm((alpha_d * ds_k), np.inf) < self.eps):
+                stay += 1
                 if (np.linalg.norm(F_k, np.inf) < 1e-3):
                     status = self.Status.HARD
                 else:
@@ -243,6 +270,11 @@ class IPM:
             self.x_k += alpha_p * dx_k
             self.lam_k += alpha_d * dlam_k
             self.s_k += alpha_d * ds_k
+            # if self.use_M:
+            # self.M = np.linalg.norm(self.x_k, np.inf)*1
+            # self.M*=0.99
+            # print(self.M)
+            # self.c[-m:] = self.M
 
         if (status == self.Status.TBD):
             status = self.Status.EXPIRED
@@ -252,35 +284,55 @@ class IPM:
             if (status.value > self.Status.HARD.value):
                 c_real = self.c
                 self.c = np.zeros_like(self.c)
-                presolve_status = self.solve(False, True)
+                presolve_status = self.solve(None, True)
                 self.c = c_real
                 if (presolve_status.value > self.Status.HARD.value):
                     status = self.Status.VIOLATED
 
+        if self.use_M:
+            if np.linalg.norm(self.x_k[-m:], np.inf) > self.terminal:
+                status = self.Status.VIOLATED
+
         total_wall_time_cost = time.time()-total_start_time
 
         print("total solving wall time =", total_wall_time_cost, "sec")
-        print("status                  =", status.name)
+        print("status                  =", status.name, ":", self.Status.spec(status))
         if (status == self.Status.DONE):
             print("primal-dual optimal solution:")
-        print("optimal objective value = %.4f" % self.f_primal(self.x_k))
-        print("numbers of iterations   =", k)
+            print("optimal objective value = %.4f" % self.f_primal(self.x_k))
+            print("numbers of iterations   =", k)
+            sol = self.x_k
+            if self.use_M:
+                sol = sol[:-m]
+            if args is not None and args.solution:
+                print("solution x              = {")
+                print(",\n".join([", ".join(["%10.4f" % j for j in sol.T[0][i:i+10]])
+                                  for i in range(0, sol.shape[0], 10)]),
+                      "\n}")
+            if optimal is not None:
+                print("given x*:||x_k-x*||_inf =", np.linalg.norm(sol-optimal, np.inf),
+                      "(optimal solution may not be unique)")
         return status
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_folder", help="a folder contain A,b,c")
+    parser.add_argument("data_folder", help="a folder containing `A.csv`, `b.csv` and `c.csv` [and `x_star.csv`]")
     parser.add_argument("--detail", required=False, default=False,
                         help="show detailed information each iteration", action="store_true")
+    parser.add_argument("--solution", required=False, default=False,
+                        help="show solution if solved", action="store_true")
+    parser.add_argument("-M", required=False, default=False,
+                        help="use the big-M form", action="store_true")
     args = parser.parse_args()
 
     ipm = IPM()
     A0 = ipm.read_file(os.path.join(args.data_folder, "A.csv"))
     b0 = ipm.read_file(os.path.join(args.data_folder, "b.csv"))
     c0 = ipm.read_file(os.path.join(args.data_folder, "c.csv"))
+    x0 = ipm.read_file(os.path.join(args.data_folder, "x_star.csv"), required=False)
 
-    ipm.init(A0, b0, c0)
-    ipm.solve(args.detail)
+    ipm.init(A0, b0, c0, use_M=args.M)
+    ipm.solve(args, optimal=x0)
 
     print("solver terminated successfully")
